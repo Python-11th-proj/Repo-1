f0cb9a936effc0da923ba09c40bb2f28da8e6f90
elleuwu <thanish1137@gmail.com>
2021-12-05 17:50:43 +0530

    snake added,optimization

--- cell_logic.py
+++ cell_logic.py
@@ -2,9 +2,10 @@ import pygame
 
 def border_check(rect,cells_added,boxsize,elem):
     global cells_around,cell_check
-    cells_around = 0
-    cell_check = rect[elem]
+    cells_around = 0 #number of cells around currently checked cell
+    cell_check = rect[elem] #currently checked cell
 
+    #rect for all surrounding areas of currently checked cell
     cell_check_left = pygame.Rect(cell_check.left-boxsize,cell_check.top,boxsize,boxsize)
     cell_check_right = pygame.Rect(cell_check.left+boxsize,cell_check.top,boxsize,boxsize)
     cell_check_top = pygame.Rect(cell_check.left,cell_check.top-boxsize,boxsize,boxsize)
@@ -15,6 +16,7 @@ def border_check(rect,cells_added,boxsize,elem):
     cell_check_topRight = pygame.Rect(cell_check.left+boxsize,cell_check.top-boxsize,boxsize,boxsize)
     cell_check_bottomRight = pygame.Rect(cell_check.left+boxsize,cell_check.top+boxsize,boxsize,boxsize)
 
+    #if surrounding rects in cells_added increase cells_around by 1
     if cell_check_left in cells_added:
         cells_around += 1
     if cell_check_right in cells_added:
@@ -36,29 +38,35 @@ def border_check(rect,cells_added,boxsize,elem):
 
 def cell_sim(rect,cells_added,boxsize):
     global cell_state
-    cell_state = []
+    cell_state = [] #list to track state of cells dead,alive,born (0,1,2)
     for elem in range(len(rect)):
         border_check(rect,cells_added,boxsize,elem)
-        if cells_around <= 1:
+        if cells_around <= 1: #less than or equal to 1 cell = cell dead (0)
             cell_state.append(0)
-        elif cells_around >= 4:
+        elif cells_around >= 4: #more than or equal to 4 cells = cell dead (0)
             cell_state.append(0)
-        elif cells_around == 2 or cells_around == 3:
-            if rect[elem] in cells_added:
-                cell_state.append(1)
-            else:
-                cell_state.append(0)
+        elif cells_around == 2 or cells_around == 3: #cell has 2 or 3 live cells = cell survives/gets born (1/2)
+            if cells_around == 2:
+                if rect[elem] in cells_added:
+                    cell_state.append(1)
+                else:
+                    cell_state.append(0)
+            elif cells_around == 3:
+                if rect[elem] in cells_added:
+                    cell_state.append(1)
+                else:
+                    cell_state.append(2)
 
 def cell_changes(rect,cells_added,boxsize,window_copy,cell_color,window_color,window):
-    birthed_cells = []
-    removed_cells = []
+    birthed_cells = [] #list to track all born cells
+    removed_cells = [] #list to track all removed cells
     cell_sim(rect,cells_added,boxsize)
     for elem in range(len(cell_state)):
-        if cell_state[elem] == 0 and border_check(rect,cells_added,boxsize,elem) == 3:
-            pygame.draw.rect(window_copy,cell_color,rect[elem])
+        if cell_state[elem] == 2: #draws a cell if cell_state = 2
+            pygame.draw.rect(window_copy,cell_color,rect[elem]) 
             pygame.draw.rect(window_copy,(20,20,20),rect[elem],1)
             birthed_cells.append(rect[elem])
-        elif cell_state[elem] == 0:
+        elif cell_state[elem] == 0: #removes a cell if cell_state = 0
             if rect[elem] in cells_added:
                 pygame.draw.rect(window_copy,window_color,rect[elem])
                 pygame.draw.rect(window_copy,(20,20,20),rect[elem],1)
@@ -69,6 +77,7 @@ def cell_changes(rect,cells_added,boxsize,window_copy,cell_color,window_color,wi
                 pass
         elif cell_state[elem] == 1:
             pass
+
     cells_added.extend(birthed_cells)
     for elem in removed_cells:
         cells_added.remove(elem)
--- displaywindow.py
+++ displaywindow.py
@@ -1,4 +1,4 @@
-import sys,pygame,time
+import sys,pygame,time,random
 
 window_size = window_width, window_height = 1800,900 #dimensions of display window
 
@@ -7,13 +7,13 @@ window_color = (50,50,50)
 grid_color = (20,20,20)
 cell_color = (230,230,230)
 
-
-cells_added = []
-grid_rect = []
+cells_added = [] #list to track added cells
+grid_rect = [] #list to track all cells in the grid
 
 from cell_logic import *
 
-def drawGrid(grid_rect):
+
+def drawGrid(grid_rect): #draws the grid and saves all the rects to grid_rect
     for x in range(window_width//boxsize):
         for y in range(window_height//boxsize):
             rect = pygame.Rect(x*boxsize,y*boxsize,boxsize,boxsize)
@@ -30,7 +30,7 @@ def cursor_hover():
     #gets the co-ordinate of the top left corner of the nearest square based on mouse position
     nearest_x = (mouse_x//boxsize)*boxsize
     nearest_y = (mouse_y//boxsize)*boxsize
-    drawGrid(grid_rect) #draw the grid everytime to remove cursor trail
+    drawGrid(grid_rect) #draw the grid everytime to remove cursor trail/(slow) optimization needed
     rect = pygame.Rect(nearest_x,nearest_y,boxsize,boxsize)
     pygame.draw.rect(window,cell_color,rect)
     
@@ -55,10 +55,14 @@ def game():
 
     add_cells = "y"
     boxsize = 30 #size of the cells in the grid
+
     font = pygame.font.SysFont("Arial", 18) #font for the fps counter
+
     grid_no = 0
     drawGrid(grid_rect) #draws the grid
     grid_no = 1
+
+
 
     phase1 = True
 
@@ -95,8 +99,11 @@ def game():
                 #if the space key is pressed the simulation is started 
                 if event.key == pygame.K_SPACE:
                     add_cells = 'n'
+                    #redraws the last cell after mouse hover is stopped
                     pygame.draw.rect(window,window_color,rect)
                     pygame.draw.rect(window,grid_color,rect,1)
+
+                    #starts the simulation
                     while phase1:
                         clock.tick(150)
                         for event in pygame.event.get():
@@ -107,7 +114,121 @@ def game():
                         window.blit(window_copy,window_rect) #displays window_copy on the display window
                         window.blit(update_fps(font),(10,0)) #displays the fps
                         pygame.display.flip() #updates the display
-                        time.sleep(0.1)
+                        time.sleep(0.2)
+
+                if event.key == pygame.K_BACKSLASH:
+                    # defining snake default position
+                    snake_position = [150, 90]
+ 
+                    # defining first 4 blocks of snake body
+                    snake_body = [  [150, 90],
+                                    [120, 90],
+                                    [90, 90],
+                                    [60, 90]
+                                   ]
+                    # fruit position
+                    fruit_position = [random.randrange(0, (1800//boxsize)) * 30,random.randrange(0, (800//boxsize)) * boxsize]
+                    fruit_spawn = True
+ 
+                    # setting default snake direction right
+                    direction = 'RIGHT'
+                    change_to = direction
+
+                    # initial score
+                    score = 0
+
+                    def show_score(choice, color, font, size,window):
+                        # creating font object score_font
+                        score_font = pygame.font.SysFont(font, size)
+                        # create the display surface object
+                        score_surface = score_font.render('Score : ' + str(score), True, color)
+                        # create a rectangular object for the score
+                        score_rect = score_surface.get_rect()
+                        # displaying text
+                        window.blit(score_surface, score_rect)
+
+                    def game_over(window):
+                        # creating font object my_font
+                        my_font = pygame.font.SysFont('times new roman', 50)
+                        # creating a text surface
+                        game_over_surface = my_font.render('Your Score is : ' + str(score), True, (255,0,0))
+                        game_over_rect = game_over_surface.get_rect()
+                        # setting position of the text
+                        game_over_rect.midtop = (window_width/2, window_height/4)
+                        window.blit(game_over_surface, game_over_rect)
+                        pygame.display.flip()
+                        time.sleep(2)
+                        pygame.quit()
+                        quit()
+
+                    while phase1:
+                        clock.tick(150)
+                        for event in pygame.event.get():
+                            if event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_UP:
+                                    change_to = 'UP'
+                                if event.key == pygame.K_DOWN:
+                                    change_to = 'DOWN'
+                                if event.key == pygame.K_LEFT:
+                                    change_to = 'LEFT'
+                                if event.key == pygame.K_RIGHT:
+                                    change_to = 'RIGHT'
+ 
+                                if change_to == 'UP' and direction != 'DOWN':
+                                    direction = 'UP'
+                                if change_to == 'DOWN' and direction != 'UP':
+                                    direction = 'DOWN'
+                                if change_to == 'LEFT' and direction != 'RIGHT':
+                                    direction = 'LEFT'
+                                if change_to == 'RIGHT' and direction != 'LEFT':
+                                    direction = 'RIGHT'
+ 
+                        # Moving the snake
+                        if direction == 'UP':
+                            snake_position[1] -= boxsize
+                        if direction == 'DOWN':
+                            snake_position[1] += boxsize
+                        if direction == 'LEFT':
+                            snake_position[0] -= boxsize
+                        if direction == 'RIGHT':
+                            snake_position[0] += boxsize
+ 
+                        # Snake body growing mechanism
+                        snake_body.insert(0, list(snake_position))
+                        if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+                            score += 10
+                            fruit_spawn = False
+                        else:
+                            snake_body.pop()
+         
+                        if not fruit_spawn:
+                            fruit_position = [random.randrange(1, (window_width//boxsize)) * boxsize,random.randrange(1, (window_height//boxsize)) * boxsize]
+         
+                        fruit_spawn = True
+                        window.fill(window_color)
+     
+                        for pos in snake_body:
+                            pygame.draw.rect(window,(0,255,0), pygame.Rect(pos[0], pos[1], boxsize, boxsize))
+         
+                        pygame.draw.rect(window,(255,0,0), pygame.Rect(fruit_position[0], fruit_position[1], boxsize, boxsize))
+ 
+                        # Game Over conditions
+                        if snake_position[0] < 0 or snake_position[0] > window_width-boxsize:
+                            game_over(window)
+                        if snake_position[1] < 0 or snake_position[1] > window_height-boxsize:
+                            game_over(window)
+     
+                        # Touching the snake body
+                        for block in snake_body[1:]:
+                            if snake_position[0] == block[0] and snake_position[1] == block[1]:
+                                game_over(window)
+     
+                        # displaying score countinuously
+                        show_score(1, (255,255,255), 'times new roman', 20,window)
+     
+                        # Refresh game screen
+                        pygame.display.update()
+                        time.sleep(0.2)
                     
         window.blit(window_copy,window_rect) #displays window_copy on the display window
         window.blit(update_fps(font),(10,0)) #displays the fps
