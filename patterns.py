aaab48878900b8742733385b5430fd907e429d87
Sanjeev Krishnaa <sanjeevkrishnaa@gmail.com>
2022-01-26 08:57:01 +0530

    patterns

--- patterns.py
+++ patterns.py
@@ -1,1 +1,5 @@
-
+def pattern():
+    repeat = [[0, 10, 0, 10],[0, 0, 10, -10]]
+    for i in len(repeat):
+        rect = pygame.Rect(900+repeat[0][i],450+repeat[1][i],boxsize,boxsize)
+        pygame.draw.rect(window,cell_color,rect)
