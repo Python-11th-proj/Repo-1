dea75f7b3960f4765bbc2fa6d50c09c81059dc63
elle uwu <jeevan.thanish@gmail.com>
2022-02-03 06:36:16 +0530

    finally done

--- patterns.py
+++ patterns.py
@@ -1,55 +1,42 @@
-import pygame 
+import pygame
+from displaywindow import *
 
-def pattern(boxsize,window,cell_color,cells_added):
-    glidergun = [[-75,1,0,-1,10,0,1,1,1,1,1,1,0,1,-1,-1,-2,-1,-1,-1,14,0,-2,-1,-1,0,1,0,-1,2,2,0,10,1,0,-1],[-40,0,1,0,0,-1,-1,-1,0,3,-2,1,1,0,1,1,1,0,-1,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,2,0,1,0]]
-    pulsar = [[0,1,1,4,1,1,2,0,0,0,0,0,-2,-1,-1,-4,-1,-1,-2,0,0,0,0,0,5,2,0,-2,0,2,0,-2,0,2,0,-2,-3,1,1,4,1,1,0,-1,-1,-4,-1,-1],[0,0,0,0,0,0,2,1,1,4,1,1,2,0,0,0,0,0,-2,-1,-1,-4,-1,-1,0,0,1,0,1,0,4,0,1,0,1,0,-5,0,0,0,0,0,2,0,0,0,0,0]]
-    flyingmachine = [[-75,-1,2,0,1,-3,0,4,0,3,-7,1,1,1,3,2,-2,3,-9,1,1,1,3,2,-4,3,-7,4,-4,2,1,-3,2,-1],[0,1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,1,1,0,1,0,1]]
-    pattern_list = flyingmachine
-
-    x=900
-    y=450
-    for i in range(len(pattern_list[0])):
-        x += pattern_list[0][i]*boxsize
-        y += pattern_list[1][i]*boxsize 
-        rect = pygame.Rect(x,y,boxsize,boxsize)
-        pygame.draw.rect(window,cell_color,rect)
-        cells_added.append(rect)
 
 def pattern1(boxsize,window,cell_color,cells_added):
     glidergun = [[-75,1,0,-1,10,0,1,1,1,1,1,1,0,1,-1,-1,-2,-1,-1,-1,14,0,-2,-1,-1,0,1,0,-1,2,2,0,10,1,0,-1],[-40,0,1,0,0,-1,-1,-1,0,3,-2,1,1,0,1,1,1,0,-1,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,2,0,1,0]]
-    pattern_list = glidergun
 
     x=900
     y=450
-    for i in range(len(pattern_list[0])):
-        x += pattern_list[0][i]*boxsize
-        y += pattern_list[1][i]*boxsize 
+    for i in range(len(glidergun[0])):
+        x += glidergun[0][i]*boxsize
+        y += glidergun[1][i]*boxsize 
         rect = pygame.Rect(x,y,boxsize,boxsize)
         pygame.draw.rect(window,cell_color,rect)
+        pygame.draw.rect(window,grid_color,rect,1)
         cells_added.append(rect)
 
 def pattern2(boxsize,window,cell_color,cells_added):
-      pulsar = [[0,1,1,4,1,1,2,0,0,0,0,0,-2,-1,-1,-4,-1,-1,-2,0,0,0,0,0,5,2,0,-2,0,2,0,-2,0,2,0,-2,-3,1,1,4,1,1,0,-1,-1,-4,-1,-1],[0,0,0,0,0,0,2,1,1,4,1,1,2,0,0,0,0,0,-2,-1,-1,-4,-1,-1,0,0,1,0,1,0,4,0,1,0,1,0,-5,0,0,0,0,0,2,0,0,0,0,0]]
-      pattern_list = pulsar
+    pulsar = [[0,1,1,4,1,1,2,0,0,0,0,0,-2,-1,-1,-4,-1,-1,-2,0,0,0,0,0,5,2,0,-2,0,2,0,-2,0,2,0,-2,-3,1,1,4,1,1,0,-1,-1,-4,-1,-1],[0,0,0,0,0,0,2,1,1,4,1,1,2,0,0,0,0,0,-2,-1,-1,-4,-1,-1,0,0,1,0,1,0,4,0,1,0,1,0,-5,0,0,0,0,0,2,0,0,0,0,0]]
 
-      x=900
-      y=450
-      for i in range(len(pattern_list[0])):
-        x += pattern_list[0][i]*boxsize
-        y += pattern_list[1][i]*boxsize 
+    x=900
+    y=450
+    for i in range(len(pulsar[0])):
+        x += pulsar[0][i]*boxsize
+        y += pulsar[1][i]*boxsize 
         rect = pygame.Rect(x,y,boxsize,boxsize)
         pygame.draw.rect(window,cell_color,rect)
+        pygame.draw.rect(window,grid_color,rect,1)
         cells_added.append(rect)
 
 def pattern3(boxsize,window,cell_color,cells_added):
-      flyingmachine = [[-75,-1,2,0,1,-3,0,4,0,3,-7,1,1,1,3,2,-2,3,-9,1,1,1,3,2,-4,3,-7,4,-4,2,1,-3,2,-1],[0,1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,1,1,0,1,0,1]]
-      pattern_list = flyingmachine
+    flyingmachine = [[-75,-1,2,0,1,-3,0,4,0,3,-7,1,1,1,3,2,-2,3,-9,1,1,1,3,2,-4,3,-7,4,-4,2,1,-3,2,-1],[0,1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,1,1,0,1,0,1]]
 
-      x=900
-      y=450
-      for i in range(len(pattern_list[0])):
-        x += pattern_list[0][i]*boxsize
-        y += pattern_list[1][i]*boxsize 
+    x=900
+    y=450
+    for i in range(len(flyingmachine[0])):
+        x += flyingmachine[0][i]*boxsize
+        y += flyingmachine[1][i]*boxsize 
         rect = pygame.Rect(x,y,boxsize,boxsize)
         pygame.draw.rect(window,cell_color,rect)
-        cells_added.append(rect)       
+        pygame.draw.rect(window,grid_color,rect,1)
+        cells_added.append(rect)   
